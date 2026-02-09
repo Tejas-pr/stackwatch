6a21a7295ba0772e8ba536b99f1a0d9132e61372
Tejas <tejas.teju02@gmail.com>
2026-02-09 11:59:22 +0530

    worker done

---
+++ apps/worker-service/index.ts
@@ -0,0 +1,68 @@
+// here I need to pick the websites from q and ping and put the status into the DB(bulk).
+import axios from "axios";
+import { prisma } from "@repo/database";
+import { XAckBulk, XReadGroup } from "@repo/redis-queue/redis-client";
+
+const REGION_ID = process.env.REGION_ID!;
+const WORKER_ID = process.env.WORKER_ID!;
+
+if (!REGION_ID) {
+    throw new Error("Region not provided");
+}
+
+if (!WORKER_ID) {
+    throw new Error("Worker not provided");
+}
+
+async function main() {
+    while (1) {
+        const response = await XReadGroup(REGION_ID, WORKER_ID);
+        if (!response) {
+            continue;
+        }
+
+        // hit the website and check status.
+        const promises = response.map(({ message }) => fetchWebsite(message.url, message.id));
+        await Promise.all(promises);
+
+        XAckBulk(REGION_ID, response.map(({ id }) => id));
+    }
+}
+
+async function fetchWebsite(url: string, website_id: string) {
+    try {
+        return new Promise<void>((resolve, reject) => {
+            const start_time = Date.now();
+
+            axios.get(url)
+                .then(async () => {
+                    const end_time = Date.now();
+                    await prisma.websiteTicks.create({
+                        data: {
+                            response_time_ms: end_time - start_time,
+                            status: "Up",
+                            regain_id: REGION_ID,
+                            website_id
+                        }
+                    });
+                    resolve();
+                })
+                .catch(async () => {
+                    const end_time = Date.now();
+                    await prisma.websiteTicks.create({
+                        data: {
+                            response_time_ms: end_time - start_time,
+                            status: "down",
+                            regain_id: REGION_ID,
+                            website_id
+                        }
+                    });
+                    resolve();
+                })
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+main();
--- packages/redis-queue/index.ts
+++ packages/redis-queue/index.ts
@@ -5,7 +5,16 @@ type Website = {
     id: string;
 }
 
+type MessageType = {
+    id: string;
+    message: {
+        id: string,
+        url: string
+    }
+}
+
 const STREAM_NAME = process.env.STREAM_NAME || "stackwatch:websites";
+const COUNT = process.env.COUNT || 5;
 
 const client = await createClient()
     .on("error", (err) => console.log("Redis Client Error: ", err))
@@ -40,6 +49,42 @@ const XAdd = async ({ url, id }: Website) => {
             url,
             id
         })
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const XReadGroup = async (consumer_group: string, worker_id: string): Promise<MessageType[] | undefined> => {
+    try {
+        const response = await client.xReadGroup(consumer_group, worker_id, {
+            key: STREAM_NAME,
+            id: '>'
+        }, {
+            COUNT: Number(COUNT)
+        });
+
+        if (!response) {
+            return [];
+        }
+
+        const messages: MessageType[] = (response as any)[0].messages;
+        return messages;
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const XAck = async (consumer_group: string, event_id: string) => {
+    try {
+        await client.xAck(STREAM_NAME, consumer_group, event_id);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const XAckBulk = async (consumer_group: string, event_ids: string[]) => {
+    try {
+        event_ids.map(event_id => XAck(consumer_group, event_id));
     } catch (e) {
         console.error(e);
     }
