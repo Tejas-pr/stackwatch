9953a4bf63cdcf1e32a78e6104a218cf5e0284bf
Tejas <tejas.teju02@gmail.com>
2026-02-13 17:23:38 +0530

    email service

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -15,4 +15,13 @@ export const auth = betterAuth({
     //         clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     //     },
     // },
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60 * 24, // 1 day
+            refreshCache: {
+                updateAge: 300 // Refresh when 5 mins remain before expiry
+            }
+        }
+    }
 });
